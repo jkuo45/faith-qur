c68ad5646a3395a150ff1e1ae8d0b2cedbb63dbc
Johnny Kuo <johnnykuo45@gmail.com>
2025-08-26 15:59:22 -0700

    initial commit

---
+++ notebooks/chapter_41_inconstancy.py
@@ -0,0 +1,26 @@
+import marimo
+
+__generated_with = "0.15.0"
+app = marimo.App(width="full")
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+    # Chapter 41: Inconstancy
+
+
+    """
+    )
+    return
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return (mo,)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ notebooks/chapter_46_the_beginning_of_christianity.py
@@ -0,0 +1,121 @@
+import marimo
+
+__generated_with = "0.15.0"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+    # Chapter 46: The Beginning of Christianity
+
+    1. The foundation of Christianity begun in Rome 🇮🇹⭐️🏛🍝. The Roman nations founded the first Church, of whom crucified Jesus of Nazareth for seeking to redeeem his people from under the Roman yoke and law.
+    2. Jesus himself was of the true blood of the ancient Canaanites and Moabites and the inhabitants of Africa.
+    """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+    2. (cont): ### Biblical Canaanites
+
+        https://www.dailymail.co.uk/sciencetech/article-8365839/Biblical-Canaanites-gradually-moved-Near-East-Southern-Levant.html
+    """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.image(
+        "https://i.dailymail.co.uk/1s/2020/05/28/16/28934148-8365839-image-a-19_1590678466469.jpg",
+        rounded=True,
+        caption="Biblical Canaanites, dailymail.co.uk",
+        alt="Map of Turkey, Iran, Iraq, Azerbaijan, Lebanon, Jordan, Israel, Egypt. photo by Chris Waybe, MailOnline, dailymail.co.uk",
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+    3. Seeking to redeem His people in those days from the pressure of the pale skin nations of Europe, Rome crucified Him according to their law.
+    4. Then Europe had peace for a long time until Mohammed the First came upon the scene and fulfilled the works of Jesus the Nazareth.
+    """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""### Diwali: Festival of Lights""")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.image(
+        """img/ch46_candles_on_the_floor.jpg""",
+        caption="Diwali: Festival of Lights, credit https://unsplash.com/@artsy_lens21",
+        alt="Diwali: Festival of Lights, credit https://unsplash.com/@artsy_lens21",
+        rounded=True,
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+    5. The holy 🕊️ teachings of Jesus was to the common people 👱‍♂️👱🏻‍♂️👱🏼‍♂️👱🏽‍♂️👱🏾‍♂️👱🏿‍♂️, to redeem them from under the great pressure of the hands of the unjust. That the rulers of the rich[ish] would not oppress the poor[ish]. Also the lion and the lamb may lay down together and neither would be be harmed when morning came 🦁🐑🌞.
+    6. These teachings were not accepted by the rulers. Neither by the rich[ish]; because they loved the principles of the 🔟 commandments.
+    7. Through the 🔟 commandments the rulers and the rich[ish] live, while the poor[ish] suffer and die 死.
+    8. The lamb 🐑🐑🐑 is the poor[ish] people, and the lion 🦁, the rulers annd the rich[ish]. And through Love, Truth, Peace, Freedom, and Justice all men are one and equal to seek their own destiny; and to worship under their own vine 🍇 and fig tree 🪾. After the principles of the holy and divine laws of their forefathers ♾️.
+    9. All nations of the earth in these modern days are seeking peace, but there is but one true and divine way that peace may be obtained in these days, and it is through:
+
+               ❤️ Love
+       
+               💙 Truth
+           
+               💜 Peace
+           
+               💚 Freedom
+           
+               🤍 Justice
+
+       Taught universally to all nations, in all lands.
+    """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""## Notes""")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.image(
+        "img/ch46_notes.jpeg",
+        width=700,
+        alt="notes for Chapter 46: The Beginning of Christrianity",
+        rounded=True,
+    )
+    return
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return (mo,)
+
+
+if __name__ == "__main__":
+    app.run()
