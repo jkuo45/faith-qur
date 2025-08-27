7af6e89cc3db6a44654b096df39ac15d28b1744e
Johnny Kuo <johnnykuo45@gmail.com>
2025-08-26 22:37:16 -0700

    fix notebook outline

--- notebooks/ch46.py
+++ notebooks/ch46.py
@@ -1,7 +1,16 @@
 import marimo
 
 __generated_with = "0.15.0"
-app = marimo.App(width="medium")
+app = marimo.App(
+    width="full",
+    app_title="Chapter 46: The Beginning of Christianity",
+)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""## Verse 1-2""")
+    return
 
 
 @app.cell
@@ -12,6 +21,12 @@ def _(mo):
     2. Jesus himself was of the true blood of the ancient Canaanites and Moabites and the inhabitants of Africa.
     """
     )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""### Map of Biblical Canaanites""")
     return
 
 
@@ -41,37 +56,16 @@ def _(mo):
 
 @app.cell
 def _(mo):
+    mo.md(r"""## Verse 3-9""")
+    return
+
+
+@app.cell
+def _(mo):
     mo.md(
         r"""
     3. Seeking to redeem His people in those days from the pressure of the pale skin nations of Europe, Rome crucified Him according to their law.
     4. Then Europe had peace for a long time until Mohammed the First came upon the scene and fulfilled the works of Jesus the Nazareth.
-    """
-    )
-    return
-
-
-@app.cell
-def _(mo):
-    mo.md(r"""### Diwali: Festival of Lights""")
-    return
-
-
-@app.cell
-def _(mo):
-    mo.image(
-        """https://raw.githubusercontent.com/jkuo45/faith-qur/main/img/ch46_candles_on_the_floor.jpg""",
-        caption="Diwali: Festival of Lights, credit https://unsplash.com/@artsy_lens21",
-        alt="Diwali: Festival of Lights, credit https://unsplash.com/@artsy_lens21",
-        rounded=True,
-        width=550,
-    )
-    return
-
-
-@app.cell
-def _(mo):
-    mo.md(
-        r"""
     5. The holy 🕊️ teachings of Jesus was to the common people 👱‍♂️👱🏻‍♂️👱🏼‍♂️👱🏽‍♂️👱🏾‍♂️👱🏿‍♂️, to redeem them from under the great pressure of the hands of the unjust. That the rulers of the rich[ish] would not oppress the poor[ish]. Also the lion and the lamb may lay down together and neither would be be harmed when morning came 🦁🐑🌞.
     6. These teachings were not accepted by the rulers. Neither by the rich[ish]; because they loved the principles of the 🔟 commandments.
     7. Through the 🔟 commandments the rulers and the rich[ish] live, while the poor[ish] suffer and die 死.
@@ -90,6 +84,24 @@ def _(mo):
 
        Taught universally to all nations, in all lands.
     """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""### Diwali: Festival of Lights""")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.image(
+        "https://raw.githubusercontent.com/jkuo45/faith-qur/main/img/ch46_candles_on_the_floor.jpg",
+        caption="Diwali: Festival of Lights, credit https://unsplash.com/@artsy_lens21",
+        alt="Diwali: Festival of Lights, credit https://unsplash.com/@artsy_lens21",
+        rounded=True,
+        width=550,
     )
     return
 
